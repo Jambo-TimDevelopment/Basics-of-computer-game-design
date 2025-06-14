01b39b4199815695fd5ce596bcab1d3b9bf8f9c6
galeev_tr <gtimur477@gmail.com>
2025-06-14 19:45:57 +0300

    initial commit of transport branch

---
+++ assets/Game Res/Transport/Code/ExampleCode.ts
@@ -0,0 +1,17 @@
+import { _decorator, Component, Node } from 'cc';
+const { ccclass, property } = _decorator;
+
+// example code for new branch
+
+@ccclass('ExampleCode')
+export class ExampleCode extends Component {
+    start() {
+
+    }
+
+    update(deltaTime: number) {
+        
+    }
+}
+
+
